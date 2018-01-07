78779bded84a428b6d365e12450f30d985ecf856
Tobias Müller <muellertobias789@gmail.com>
2018-01-07 17:44:05 +0100

    changed to smartpointers

--- OpenCLExtended/Data/ComputingParameters.h
+++ OpenCLExtended/Data/ComputingParameters.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <CL\cl.hpp>
+#include <memory>
 
 typedef struct {
-	cl::Kernel kernel;
-	cl::CommandQueue queue;
-	cl::ImageGL texture;
-	cl::size_t<3> dims;
+	std::unique_ptr<cl::Kernel> Kernel;
+	std::unique_ptr<cl::CommandQueue> Queue;
+	std::unique_ptr<cl::Memory> Texture; //cl::ImageGL texture;
+	cl::size_t<3> Dimensions;
 } ComputingParameters;
