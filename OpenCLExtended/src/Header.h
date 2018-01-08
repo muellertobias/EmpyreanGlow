68669ef690dedc87aa59045f5650142d4e3c56c0
Tobias Müller <muellertobias789@gmail.com>
2018-01-08 19:26:50 +0100

    structure

---
+++ OpenCLExtended/src/Header.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Kernel/KernelConfig.h"
+#include "Kernel/KernelFactory.h"
+#include "Kernel/Kernel.h"
+
+void test() 
+{
+	using namespace OpenCLExt::Kernel;
+
+	int argument = 5;
+	KernelConfig* c = new KernelConfig("test", "entry", 5);
+	c->setArguments(0, sizeof(argument), &argument);
+
+	KernelFactory* factory = new KernelFactory();
+	//Kernel kernel = factory.create(c, )
+
+	delete(factory);
+	delete(c);
+}
